031ad207a31e37cf4b1fca697b539a4b1f7ac9da
Oughlane <oughlanemohammed72@gmail.com>
2025-09-08 06:59:24 +0100

    pair impair

---
+++ Tableaux/Challenge12.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main (){
+int n,i,j,pair=0;
+    printf("Donner la taille du tableau : ");
+    scanf("%d", &n);
+    int t[n];
+    printf("\nEntrer %d elements :\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &t[i]);
+    }
+    printf("\nLes elements pair sont:");
+    for(i=0;i<n;i++){
+        if(t[i] %2==0){
+               printf("\n%d",t[i]);
+        }
+    }
+    return 0;
+}
