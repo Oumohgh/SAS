806359271f6408471fd01f28b504a92945c1f6d2
Oughlane <oughlanemohammed72@gmail.com>
2025-09-09 07:45:36 +0100

    concaterante

---
+++ StringS/Challenge3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main (){
+char str1[20];
+char str2[20];
+strcpy(str1,"Mohammed");
+strcpy(str2,"Oughlane");
+strcat(str1,str2);
+printf("%s",str1);
+return 0;
+
+}
