e1149b15e14267560ec8b917842c49ef8f0e038a
Oughlane <oughlanemohammed72@gmail.com>
2025-09-10 08:21:46 +0100

    7

--- StringS/Challenge7.c
+++ StringS/Challenge7.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
 #include<string.h>
-
-int main
+int main(){
+   int i;
+   char string[50];
+   printf("Entrer une chaine des cara:");
+   scanf("%[^\n]",string);
+   for(i=0;string[i]!='\0';i++){
+      if(string[i]>=97&&string[i]<=122){
+         string[i]=string[i]-32;
+      }
+   }
+   printf("\nle chaine modifie :");
+   puts(string);
+   return 0;
+}
